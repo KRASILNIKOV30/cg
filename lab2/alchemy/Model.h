a77ba485413bacf08b5d2dea5a19fb343fd09e3a
Bogdan Krasilnikov <bogdan.krasilnikov@ispring.com>
2025-02-27 13:37:01 +0300

    lab2 написал модель алхимии

--- lab2/alchemy/Model.h
+++ lab2/alchemy/Model.h
@@ -1,6 +1,8 @@
 #pragma once
 #include "Element.h"
-
+#include "../../lib/signals/SignallingValue.h"
+#include <ranges>
+#include <cmath>
 #include <unordered_map>
 #include <vector>
 
@@ -8,6 +10,12 @@ struct Point
 {
 	int x;
 	int y;
+};
+
+struct Size
+{
+	int width;
+	int height;
 };
 
 struct ElementModel
@@ -27,9 +35,14 @@ struct CanvasElement
 	Point position;
 };
 
+constexpr int ACTION_DISTANCE = 50;
+
 class Model
 {
 public:
+	using UpdateSignal = EmptySignal;
+	using UpdateSlot = UpdateSignal::slot_type;
+
 	explicit Model(std::vector<Element> const& elements)
 	{
 		for (const auto& element : elements)
@@ -40,9 +53,126 @@ public:
 		}
 	}
 
+	[[nodiscard]] ScopedConnection DoOnUpdate(UpdateSlot const& slot)
+	{
+		return m_updateSignal.connect(slot);
+	}
+
+	void ForEachCanvasElement(std::function<void(CanvasElement const&)> const& callback)
+	{
+		for (const auto& element : std::views::values(m_canvasElements))
+		{
+			callback(element);
+		}
+	}
+
+	void ForEachElement(std::function<void(ElementModel const&)> const& callback)
+	{
+		for (const auto& element : std::views::values(m_elements))
+		{
+			if (element.open)
+			{
+				callback(element);
+			}
+		}
+	}
+
+	void Move(size_t const id, Point const& position)
+	{
+		m_canvasElements.at(id).position = position;
+		m_updateSignal();
+	}
+
+	void Drop(size_t const id, Point const& position)
+	{
+		m_canvasElements.at(id).position = position;
+		CheckCombinations(id);
+		CheckTrash(id);
+		m_updateSignal();
+	}
+
+	void CreateElement(ElType const type)
+	{
+		AddElement(type, Point{ 100, 100 });
+	}
+
 private:
+	void CheckCombinations(size_t const targetId)
+	{
+		const auto nearbyElementIds = GetNearbyElementIds(targetId);
+		for (const auto id : nearbyElementIds)
+		{
+			TryCreateNewElement(targetId, id);
+		}
+	}
+
+	void CheckTrash(size_t const id)
+	{
+		if (ArePointsNearby(m_trashPosition, m_canvasElements.at(id).position))
+		{
+			m_canvasElements.erase(id);
+		}
+	}
+
+	void TryCreateNewElement(size_t const firstId, size_t const secondId)
+	{
+		const auto firstEl = m_canvasElements.at(firstId);
+		const auto secondEl = m_canvasElements.at(secondId);
+		const auto typeA = firstEl.type;
+		const auto typeB = secondEl.type;
+		if (m_combinations.at(typeA).contains(typeB))
+		{
+			m_canvasElements.erase(firstId);
+			m_canvasElements.erase(secondId);
+			const auto newElementPos = Point{
+				(firstEl.position.x + secondEl.position.x) / 2,
+				(firstEl.position.y + secondEl.position.y) / 2
+			};
+			const auto newElementType = m_combinations.at(typeA).at(typeB);
+			m_elements.at(newElementType).open = true;
+			AddElement(newElementType, newElementPos);
+		}
+	}
+
+	void AddElement(ElType type, Point const& position)
+	{
+		const auto id = m_id++;
+		const auto model = m_elements.at(type);
+		m_canvasElements.emplace(id, CanvasElement{
+			.id = id,
+			.type = type,
+			.position = position,
+			.img = model.img,
+			.name = model.name,
+		});
+	}
+
+	std::vector<size_t> GetNearbyElementIds(size_t const targetId)
+	{
+		const auto target = m_canvasElements.at(targetId);
+		std::vector<size_t> result;
+		for (const auto& [id, element] : m_canvasElements)
+		{
+			if (id != targetId && ArePointsNearby(element.position, target.position))
+			{
+				result.push_back(id);
+			}
+		}
+
+		return result;
+	}
+
+	static bool ArePointsNearby(Point const& p1, Point const& p2)
+	{
+		const auto distance = std::hypot(p1.x - p2.x, p1.y - p2.y);
+		return distance <= ACTION_DISTANCE;
+	}
+
+private:
+	UpdateSignal m_updateSignal;
 	std::unordered_map<ElType, ElementModel> m_elements{};
 	std::unordered_map<size_t, CanvasElement> m_canvasElements{};
 	std::unordered_map<ElType, std::unordered_map<ElType, ElType>> m_combinations{};
 	Point m_trashPosition = { 300, 600 };
+	size_t m_id = 0;
 };
